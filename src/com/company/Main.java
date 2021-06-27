3a3ef71b13424e2190ec0855f83e8ffd29af06a9
fff4k8 <4k8@mail.ru>
2021-06-27 04:31:48 +0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Vector;
+
+public class Main {
+
+    public static void main(String[] args) throws InterruptedException {
+
+
+        Data data = new Data();
+        Thread sender = new Thread(new Sender(data));
+        Thread receiver = new Thread(new Receiver(data));
+
+        sender.start();
+        receiver.start();
+
+
+    }
+}
